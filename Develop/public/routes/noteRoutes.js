fde01aa06fb8dea2055da6721e7e970e791e98c6
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-03 02:33:05 -0600

    get method for notes

--- Develop/public/routes/noteRoutes.js
+++ Develop/public/routes/noteRoutes.js
@@ -1,2 +1,12 @@
 const router = require("express").Router();
 const dbHandler = require(".../db/dbHandler");
+
+router.get("/notes", (req, res) => {
+    dbHandler.readNotes()
+    .then((response) => {
+        return res.json(response)
+    })
+    .catch((err) => {
+        res.status(500).json(err)
+    });
+})
