89ffa4f24f89f0cd148ea3ba64be58e55e05bb67
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-03 02:38:08 -0600

    get methods for html

--- Develop/public/routes/routes.js
+++ Develop/public/routes/routes.js
@@ -1,1 +1,17 @@
 const router = require("express").Router();
+
+router.get("/notes", (req, res) => {
+    res.sendFile("./notes.html")
+    .catch((err) => {
+        res.status(500).json(err)
+    });
+})
+
+router.get("/", (req, res) => {
+    res.sendFile("./index.html")
+    .catch((err) => {
+        res.status(500).json(err)
+    });
+})
+
+module.exports = router;
