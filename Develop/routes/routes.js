241f92199e92d18c85f34ce1983dda4ebbc938a9
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-03 02:52:28 -0600

    attempt to fix routes

--- Develop/routes/routes.js
+++ Develop/routes/routes.js
@@ -1,14 +1,15 @@
 const router = require("express").Router();
+const path = require('path');
 
 router.get("/notes", (req, res) => {
-    res.sendFile("./notes.html")
+    res.sendFile(path.join(__dirname, "../public/assets/notes.html"))
     .catch((err) => {
         res.status(500).json(err)
     });
 })
 
 router.get("/", (req, res) => {
-    res.sendFile("./index.html")
+    res.sendFile(path.join(__dirname, "../public/assets/index.html"))
     .catch((err) => {
         res.status(500).json(err)
     });
