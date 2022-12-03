bc01dd0de575ec412e6c4f8116a911e9e99e0802
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-03 00:12:24 -0600

    added function that reads stored notes

--- Develop/db/dbHandler.js
+++ Develop/db/dbHandler.js
@@ -1,2 +1,9 @@
 const fs = require("fs");
 const path = require("path");
+
+function readNotes() {
+    fs.readFile("./db.json", "utf-8", (err, data) => {
+        console.log(JSON.parse(data));
+    });
+}
+readNotes();
