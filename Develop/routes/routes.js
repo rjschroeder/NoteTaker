a38f131d5dc2fb906fc0e0478bddbeda0c596c10
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-03 02:56:49 -0600

    fixed errors
    incorrect file directory and improper catch statements

--- Develop/routes/routes.js
+++ Develop/routes/routes.js
@@ -2,17 +2,11 @@ const router = require("express").Router();
 const path = require('path');
 
 router.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/assets/notes.html"))
-    .catch((err) => {
-        res.status(500).json(err)
-    });
+    res.sendFile(path.join(__dirname, "../public/notes.html"))
 })
 
 router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/assets/index.html"))
-    .catch((err) => {
-        res.status(500).json(err)
-    });
+    res.sendFile(path.join(__dirname, "../public/index.html"))
 })
 
 module.exports = router;
